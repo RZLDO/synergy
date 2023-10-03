7f3b57f552ad89de351f76b060ef12ea7ae6c7a8
Rzldo <ezas4531@gmail.com>
2023-10-03 15:18:59 +0700

    add splash screen and navigation

--- app/src/main/java/id/synergy/chalenge3/MainActivity.kt
+++ app/src/main/java/id/synergy/chalenge3/MainActivity.kt
@@ -10,8 +10,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(_binding.root)
-        supportFragmentManager.beginTransaction()
-            .addToBackStack(null)
-            .commit();
     }
 }
